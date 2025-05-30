ee32f3b669ce81fa0626393daf65ee8961386aca
Victolou <102380976+Victolou@users.noreply.github.com>
2025-05-30 19:47:27 +0200

    Remove and reorganize files

--- crusher.gd
+++
@@ -1,6 +0,0 @@
-class_name Crusher extends RigidBody2D
-
-@export var move_speed: float = 300.0
-
-func _process(delta: float) -> void:
-	position.x -= move_speed * delta
--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -3,6 +3,7 @@ class_name Main extends Node2D
 @onready var player: Player = $player
 @onready var crusher: Crusher = $crusher
 
+
 func _ready() -> void:
 	print(player.power_advantage)
 
